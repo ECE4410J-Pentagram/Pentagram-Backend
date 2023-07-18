ec25faffd4ce5ef9aa03318d7e2b0180177c9ab6
Patrick Li <baichuanli@yahoo.com>
2023-07-18 13:18:20 +0800

    feat: use redis for user login authentication

--- DBModel/Friends.py
+++ DBModel/Friends.py
@@ -1,10 +1,11 @@
-from peewee import CharField, ForeignKeyField
+from peewee import BooleanField, ForeignKeyField
 from .db import BaseModel
 
 
 class Friend(BaseModel):
     user1 = ForeignKeyField("User", backref="friends")
     user2 = ForeignKeyField("User", backref="friends")
+    is_accepted = BooleanField(default=False)
 
     def __str__(self):
         return f"User: {self.user1} with public key: {self.user2}"
--- utils/login.py
+++ utils/login.py
@@ -1,24 +1,36 @@
-import jwt
+"""Login and token management"""
+import secrets
 from utils.models import BaseUser
 from config import config
 from fastapi import Header, HTTPException
 from datetime import datetime
 from pydantic import BaseModel
+from DBModel.User import User
+import redis
 
-class Token(BaseModel):
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+def random_token():
+    return secrets.token_urlsafe(32)
+
+class TokenPayload(BaseUser):
     login_time: float
-    user: BaseUser
 
 def createToken(user: BaseUser) -> str:
-    payload = Token(login_time=datetime.now().timestamp(), user=user).dict()
+    payload = TokenPayload(login_time=datetime.now().timestamp(), username=user.username).json()
     print(payload)
-    encoded = jwt.encode(payload, config.JWT_SECRET, algorithm='HS256')
-    return encoded
+    token = random_token()
+    r.set(token, payload)
+    return token
 
 def loggedIn(Authorization: str = Header(...)):
-    try:
-        token = jwt.decode(Authorization, config.JWT_SECRET, algorithms=['HS256'])
-        token = Token(**token)
-        return token.user
-    except:
+    payload = r.get(Authorization)
+    if payload is None:
         raise HTTPException(status_code=401, detail="Invalid token")
+    payload = TokenPayload.parse_raw(payload)
+    
+    username = payload.username
+    user = User.get_or_none(BaseUser.username == username)
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+    return user
