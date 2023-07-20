f3b2b78c8346b6a5204bb7ae658f0efb3cd0cd33
baixing <baixing@baixings-MacBook-Air-3.local>
2023-07-20 18:42:10 +0800

    deploy: change postgres service name

--- config/prod.py
+++ config/prod.py
@@ -4,5 +4,5 @@ class Config:
     PDB_NAME = environ.get("POSTGRES_DB")
     PDB_USER = environ.get("POSTGRES_USER")
     PDB_PASS = environ.get("POSTGRES_PASSWORD")
-    PDB_HOST = "postgres"
-    PDB_PORT = 5432
+    PDB_HOST = environ.get("POSTGRES_HOST")
+    PDB_PORT = environ.get("POSTGRES_PORT")
