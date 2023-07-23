1facb120d59f6bee869b6f164360fd745a3a01c9
baixing <baixing@baixings-MacBook-Air-3.local>
2023-07-23 19:49:38 +0800

    feat: pooled database connection

--- DBModel/db.py
+++ DBModel/db.py
@@ -1,12 +1,15 @@
 from peewee import PostgresqlDatabase, Model
 from config import config
+from playhouse.pool import PooledPostgresqlDatabase
 
-psql_db = PostgresqlDatabase(
+psql_db = PooledPostgresqlDatabase(
     database=config.PDB_NAME,
     user=config.PDB_USER,
     password=config.PDB_PASS,
     host=config.PDB_HOST,
-    port=config.PDB_PORT
+    port=config.PDB_PORT,
+    max_connections=8,
+    stale_timeout=300
 )
 
 class BaseModel(Model):
