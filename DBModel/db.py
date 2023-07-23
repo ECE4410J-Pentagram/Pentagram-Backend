1bcbdebe60ad194ef45981f45fc931fbda2318d1
baixing <baixing@baixings-MacBook-Air-3.local>
2023-07-23 22:28:23 +0800

    fix: peewee with async

--- DBModel/db.py
+++ DBModel/db.py
@@ -1,4 +1,6 @@
 from peewee import PostgresqlDatabase, Model
+from contextvars import ContextVar
+import peewee
 from config import config
 from playhouse.pool import PooledPostgresqlDatabase
 
@@ -11,6 +13,22 @@ psql_db = PooledPostgresqlDatabase(
     max_connections=8,
     stale_timeout=300
 )
+
+
+db_state_default = {"closed": None, "conn": None, "ctx": None, "transactions": None}
+db_state = ContextVar("db_state", default=db_state_default.copy())
+class PeeweeConnectionState(peewee._ConnectionState):
+    def __init__(self, **kwargs):
+        super().__setattr__("_state", db_state)
+        super().__init__(**kwargs)
+
+    def __setattr__(self, name, value):
+        self._state.get()[name] = value
+
+    def __getattr__(self, name):
+        return self._state.get()[name]
+
+psql_db._state = PeeweeConnectionState()
 
 class BaseModel(Model):
     """A base model that will use our Postgresql database"""
