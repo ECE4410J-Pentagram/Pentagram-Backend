1217dc0d813393a974213ebe8dacff30c9dc3241
Patrick Li <baichuanli@yahoo.com>
2023-07-24 17:51:41 +0800

    fix: more atomicity

--- renderers/login.py
+++ renderers/login.py
@@ -4,6 +4,7 @@ from utils.login import createToken, check_key
 import pydantic
 from DBModel.Device import Device
 from utils.peewee import get_db
+from DBModel.db import psql_db
 
 class TokenResponse(pydantic.BaseModel):
     Authorization: str
@@ -11,7 +12,13 @@ class TokenResponse(pydantic.BaseModel):
 loginRouter = APIRouter(prefix="/api/login", tags=["login"])
 @loginRouter.post("/", response_model=TokenResponse)
 async def login(device: LoginDevice, db = Depends(get_db)):
-    db_device = Device.get_or_none(Device.name == device.name)
+
+    @psql_db.atomic()
+    def _login():
+        db_device = Device.get_or_none(Device.name == device.name)
+        return db_device
+
+    db_device = _login()
     if db_device == None:
         raise HTTPException(status_code=401, detail="Invalid credentials")
     if not check_key(device.key, db_device.key_hash):
