05c09aaaa2d7381f7cd5c1a7adbd745153dbc095
Patrick Li <baichuanli@yahoo.com>
2023-08-07 17:58:18 +0800

    feat: cache the contents if needed

--- renderers/static.py
+++ renderers/static.py
@@ -5,8 +5,14 @@ router = APIRouter(tags=["static"])
 
 @router.get("/public/{path}")
 async def public(path: str):
-    return FileResponse(f"public/{path}")
+    headers = {
+            "Cache-Control": "public, max-age=31536000",
+            }
+    return FileResponse(f"public/{path}", headers=headers)
 
 @router.get("/download/")
 async def download():
-    return FileResponse("public/cryptex.apk")
+    headers = {
+            "Cache-Control": "public, max-age=31536000",
+            }
+    return FileResponse("public/cryptex.apk", headers=headers)
